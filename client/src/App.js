86e87e0275f073c875104ca29615654eabd2c89a
Chris Kavanagh <ckava3@gmail.com>
2018-12-26 08:39:00 -0500

    Redux Provider to Index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import store from './store';
-import { Provider } from 'react-redux';
+//import store from './store';
+//import { Provider } from 'react-redux';
 import ShoppingList from './components/ShoppingList';
 import AppNavbar from './components/AppNavbar';
 import ItemModal from './components/ItemModal';
@@ -9,16 +9,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <Provider store={store}>
+    return (      
         <div className="App">
           <AppNavbar />
           <Container>
             <ItemModal />
             <ShoppingList />
           </Container>
-        </div>
-      </Provider>
+        </div>      
     );
   }
 }
